8dd36cff62696b990690b444a25e53551d820510
MikhailIshmuratov <mailto:myx-myx@mail.ru>
2018-09-05 15:58:23 +0500

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,17 @@
 package ru.job4j.calculate;
 
+/**
+ * Тестовый вывод данных 
+ * @author Mikhail Ismuratov
+ * @since 05.09.2018
+ * @version 1.0
+ */
 public class Calculate {
+	/**
+	 * print "Hello World"
+	 * @param args - имя параметра
+	 * @return вывод текста
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Mikhail Ishmuratov (mailto:myx-myx@mail.ru)
+ * @version $Id$
+ * @since 05.09.2018
+ */
+package ru.job4j.calculate;
